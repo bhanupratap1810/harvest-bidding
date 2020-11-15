6ccbd5415e12714df17be6daea6b98709a5341af
bhanupratap1810 <bhanupratapbd1@gmail.com>
2020-11-16 03:16:16 +0530

    last commit

---
+++ build/precache-manifest.01b468ed0a77ca8a4371e8fcde8ffa1a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5606701b5bf2f2b838404675b8885d4b",
+    "url": "/harvest-bidding/index.html"
+  },
+  {
+    "revision": "a3f19cdbdd391efd998e",
+    "url": "/harvest-bidding/static/js/2.1d909945.chunk.js"
+  },
+  {
+    "revision": "749e0c65135facf96309",
+    "url": "/harvest-bidding/static/js/main.961b5c12.chunk.js"
+  },
+  {
+    "revision": "023974ee4e3a52fa1b34",
+    "url": "/harvest-bidding/static/js/runtime-main.09693eac.js"
+  }
+]);
