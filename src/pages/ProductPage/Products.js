db8f83ad19bb367fc3c0f3378fb3b178a1b12d38
bhanupratap1810 <bhanupratapbd1@gmail.com>
2020-11-16 02:55:59 +0530

    bh

--- src/pages/ProductPage/Products.js
+++ src/pages/ProductPage/Products.js
@@ -1,49 +1,49 @@
 export const Data = [
   {
     title: "Apple",
-    component: "Rs 40",
+    component: "Rs 95",
     imgurl:
       "https://images.unsplash.com/photo-1567306226416-28f0efdc88ce?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max"
   },
   {
     title: "Mango",
-    component: "Rs 40",
+    component: "Rs 50",
     imgurl:
       "https://www.purina.ca/sites/g/files/auxxlc601/files/styles/facebook_share/public/Purina-Article-Can-Dogs-Eat-Mangoes_500x300.png?itok=0VgQFq0E"
   },
   {
     title: "Banana",
-    component: "Rs 40",
+    component: "Rs 20",
     imgurl:
       "https://images.newscientist.com/wp-content/uploads/2019/01/31134057/gettyimages-86304874.jpg"
   },
   {
     title: "Orange",
-    component: "Rs 40",
+    component: "Rs 45",
     imgurl:
       "https://www.healthyfoodhouse.com/wp-content/uploads/2013/03/orange-king-of-fruits.jpg"
   },
   {
     title: "Lemon",
-    component: "Rs 40",
+    component: "Rs 70",
     imgurl:
       "https://metro.co.uk/wp-content/uploads/2019/09/GettyImages-610885220.jpg?quality=90&strip=all"
   },
   {
     title: "Onion",
-    component: "Rs 40",
+    component: "Rs 50",
     imgurl:
       "https://images.financialexpress.com/2020/01/2-179.jpg?w=1200&h=800&imflag=true"
   },
   {
     title: "Potato",
-    component: "Rs 40",
+    component: "Rs 15",
     imgurl:
       "https://www.commodityonline.com/uploads/b2b-commodity/2016/potato-8705161064.jpg"
   },
   {
     title: "Tomato",
-    component: "Rs 40",
+    component: "Rs 30",
     imgurl: "https://static.toiimg.com/photo/68903018.cms"
   }
 ];
